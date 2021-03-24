56aa7b45b07b774aee37fd73cbc674fabff50dc1
mocha <syda07@gmail.com>
2021-03-25 01:07:12 +0900

    🎉 typescript 로 만든 quiz app (기능끝)

---
+++ src/API.ts
@@ -0,0 +1,34 @@
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  difficulty: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+// 타입 상속받은뒤, 추가
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};
